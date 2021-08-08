34b160a6ea731876cb918d80ab77a168d08f9618
Yorkie Fan <terahowell@gmail.com>
2021-08-08 15:52:12 -0400

    updated utils and index js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is your project name?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'What is your project description?'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project'
+},
+{
+    type: 'input',
+    name: 'usage',
+},
+{
+    type: 'input',
+    name: 'contributing',
+},
+{
+    type: 'input',
+    name: 'tests',
+},
+{
+    type: 'input',
+    name: 'license',
+    message:'What license would you like to choose?',
+    choices: [
+        'GNU General Public License v3.0'
+      ],
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, generateMarkdown(data));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(answer => generateMarkdown(answer))
+    .then(generateReadme => writeToFile("README.md", generateReadme));
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const inquirer = require("inquirer");
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  inquirer
+  .prompt({
+    name:'action',
+    type: 'list',
+    message:'What license would you like to choose?',
+    choices: [
+      'GNU General Public License v3.0'
+    ],
+  })
+  .then((answer) =>{
+    switch (answer.action){
+      case 'GNU General Public License v3.0';
+        break;
+      case '',
+    }
+  })
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +34,17 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  readmeContents= `# ${data.title}
+  ## Description: ${data.description}
+  ## Table of Contents
+  ## installation
+  ## ${data.usage}'
+  contributing
+  tests 
+  ##Questions:
+  ## License
 `;
+  readmeContents = readmeContents + 
 }
 
 module.exports = generateMarkdown;
